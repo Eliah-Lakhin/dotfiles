3278a975714834c70f0cf6db59d69676bf5c0439
Ilya Lakhin <eliah.lakhin@gmail.com>
2014-03-31 02:28:03 +0700

    Various configs

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+pushd ./utils/offirmo-shell-lib/bin/
+source osl_lib_init.sh
+source osl_lib_file.sh
+popd
+
+OSL_FILE_find_relative_path "$HOME" "$(pwd)" 
+home_rel=$return_value
+
+dotfiles=( "bashrc" "gitconfig" "linerc" "profile" "vim" "vimrc" "screenrc" )
+for dotfile in "${dotfiles[@]}"
+do
+ rm -rf $HOME/.$dotfile
+ ln -s $home_rel/custom/$dotfile $HOME/.$dotfile
+done
+
---
+++ misc/hterm-prefs.js
@@ -0,0 +1,9 @@
+term_.prefs_.importFromJson({
+  "audible-bell-sound": "",
+  "cursor-blink": true,
+  "enable-bold-as-bright": false,
+  "font-family": "monospace",
+  "font-size": 13,
+  "scroll-on-keystroke": false
+});
+
