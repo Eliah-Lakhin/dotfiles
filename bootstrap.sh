ad8ea3bdcb23c127df9d2789eab7d13d1fd82a80
Ilya Lakhin <eliah.lakhin@gmail.com>
2014-04-04 02:05:00 +0700

    Vim ctags

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,13 @@ home_rel=$return_value
 dotfiles=( "bashrc" "gitconfig" "linerc" "profile" "vim" "vimrc" "screenrc" )
 for dotfile in "${dotfiles[@]}"
 do
- rm -rf $HOME/.$dotfile
- ln -s $home_rel/custom/$dotfile $HOME/.$dotfile
+  rm -rf $HOME/.$dotfile
+  ln -s $home_rel/custom/$dotfile $HOME/.$dotfile
 done
-mkdir -p $HOME/.vimbackup
+
+dirs=( "vimbackup" )
+for dir in "${dirs[@]}"
+do
+  mkdir -p $HOME/.$dir
+done
 
